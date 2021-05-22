1e6f23acf1c5aa1c1b64a60276e898036f11f59d
NikosSpyropoulos <nikspyropoylos@gmail.com>
2021-05-22 08:12:43 +0300

    Changes in table symbols. Shows the temp variables

--- test.c
+++ test.c
@@ -2,17 +2,16 @@
 
 int main()
 {
-int num, div, i, range, x, T_0, T_1, T_2, T_3;
+int a, b, c, T_0;
 L_0: 
-L_1: ;
-L_2: 
-L_3: v = d; // (:=	d	_	v	)
-L_4: T_0 = d+0; // (+	d	0	T_0	)
-L_5: f = T_0; // (:=	T_0	_	f	)
-L_6: T_3 = num/div; // (/	num	div	T_3	)
-L_7: T_2 = div*T_3; // (*	div	T_3	T_2	)
-L_8: T_1 = num-T_2; // (-	num	T_2	T_1	)
-L_9: return T_1; // (retv	T_1	_	_	)
+L_1: T_0 = a+1; // (+	a	1	T_0	)
+L_2: c = T_0; // (:=	T_0	_	c	)
+L_3: ik = r; // (:=	r	_	ik	)
+L_4: return c; // (retv	c	_	_	)
+L_5: ;
+L_6: 
+L_7: a = 1; // (:=	1	_	a	)
+L_8: L_9: b = f; // (:=	f	_	b	)
 L_10: return 0; // (halt	_	_	_	)
 L_11: ;
 }
